d498af3ae006220086744bbc23131baa7785d76c
Luis Henrique Silva Ferreira <56833636+Henricknen@users.noreply.github.com>
2021-06-13 23:35:36 -0300

    Destrutor

---
+++ ComposicaoNotaFiscal/ItemNotaFiscal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComposicaoNotaFiscal
+{
+    class ItemNotaFiscal
+    {
+        public int QtdeProduto { get; set; }
+
+        public ItemNotaFiscal(int qtdeProduto)
+        {
+            QtdeProduto = qtdeProduto;
+        }
+        ~ItemNotaFiscal()
+        {
+            Console.WriteLine("Destrutor do item da nota fiscal");
+        }
+    }
+}
---
+++ ComposicaoNotaFiscal/Nota Fiscal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComposicaoNotaFiscal
+{
+    class NotaFiscal
+    {
+        public int NumeroNotaFiscal { get; set; }
+        public string Data { get; set; }
+        public List<ItemNotaFiscal> Item { get; set; }
+
+        public NotaFiscal(int numeroNotaFiscal, string data, List<ItemNotaFiscal> item)
+        {
+            NumeroNotaFiscal = numeroNotaFiscal;
+            Data = data;
+            Item = item;
+        } 
+        ~NotaFiscal()
+        {
+            Item = null;
+            Console.WriteLine("Destrutor da Nota fiscal");
+        }
+    }
+}
